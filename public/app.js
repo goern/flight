8fbeb891f413f6b8b5d69effd862dd226aa38697
Christoph Görn <goern@redhat.com>
2016-07-10 22:14:33 +0200

    add airport searchlet

--- public/app.js
+++ public/app.js
@@ -4,8 +4,24 @@
 var app = angular.module('flight', [
 ]);
 
-app.controller('instantSearch',function($scope,$http){
+app.controller('instantSearch',function($scope,$http) {
   $http.get('List_of_ICAO_aircraft_type_designators.json').success(function(data, status, headers, config) {
+    $scope.items = data;
+  }).error(function(data, status, headers, config) {
+    console.log("No data found..");
+  });
+});
+
+app.controller('airport_search',function($scope,$http) {
+  $http.get('/api/airports').success(function(data, status, headers, config) {
+    $scope.items = data;
+  }).error(function(data, status, headers, config) {
+    console.log("No data found..");
+  });
+});
+
+app.controller('flightplan_search',function($scope,$http) {
+  $http.get('/api/flightplans').success(function(data, status, headers, config) {
     $scope.items = data;
   }).error(function(data, status, headers, config) {
     console.log("No data found..");
@@ -15,7 +31,7 @@ app.controller('instantSearch',function($scope,$http){
 app.filter('searchFor', function(){
   return function(arr, searchString){
     if (!searchString){
-      return arr;
+      return;
     }
 
     var result = [];
@@ -24,6 +40,26 @@ app.filter('searchFor', function(){
     angular.forEach(arr, function(item){
       if ((item.ICAO.toLowerCase().indexOf(searchString) !== -1) ||
           (item.Description.toLowerCase().indexOf(searchString) !== -1)) {
+        result.push(item);
+      }
+    });
+
+    return result;
+  };
+});
+
+app.filter('airportFilter', function(){
+  return function(arr, searchString){
+    if (!searchString){
+      return;
+    }
+
+    var result = [];
+    searchString = searchString.toLowerCase();
+
+    angular.forEach(arr, function(item){
+      if ((item.city.toLowerCase().indexOf(searchString) !== -1) ||
+          (item.icao.toLowerCase().indexOf(searchString) !== -1)) {
         result.push(item);
       }
     });
