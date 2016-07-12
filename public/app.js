c78243a725ee18605b262a7b40fd9feb8f1b67a1
Christoph Görn <goern@redhat.com>
2016-07-12 11:26:37 +0200

    major UI rework

--- public/app.js
+++ public/app.js
@@ -1,34 +1,31 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-var app = angular.module('flight', [
-]);
-
-app.controller('instantSearch',function($scope,$http) {
+angular.module('flight', [
+  'datatables', 'ngResource'
+])
+.controller('instantSearch',function($scope,$http) {
   $http.get('List_of_ICAO_aircraft_type_designators.json').success(function(data, status, headers, config) {
     $scope.items = data;
   }).error(function(data, status, headers, config) {
     console.log("No data found..");
   });
-});
-
-app.controller('airport_search',function($scope,$http) {
+})
+.controller('airport_datatable', AirportsWithPromiseCtrl)
+.controller('airport_search',function($scope,$http) {
   $http.get('/api/airports').success(function(data, status, headers, config) {
     $scope.items = data;
   }).error(function(data, status, headers, config) {
     console.log("No data found..");
   });
-});
-
-app.controller('flightplan_search',function($scope,$http) {
+})
+.controller('flightplan_search',function($scope,$http) {
   $http.get('/api/flightplans').success(function(data, status, headers, config) {
     $scope.items = data;
   }).error(function(data, status, headers, config) {
     console.log("No data found..");
   });
-});
-
-app.filter('searchFor', function(){
+})
+.filter('searchFor', function() {
   return function(arr, searchString){
     if (!searchString){
       return;
@@ -46,9 +43,8 @@ app.filter('searchFor', function(){
 
     return result;
   };
-});
-
-app.filter('airportFilter', function(){
+})
+.filter('airportFilter', function() {
   return function(arr, searchString){
     if (!searchString){
       return;
@@ -67,3 +63,31 @@ app.filter('airportFilter', function(){
     return result;
   };
 });
+
+
+function AirportsWithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q) {
+  var vm = this;
+  vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+      var defer = $q.defer();
+      $http.get('/api/airports').then(function(result) {
+          defer.resolve(result.data);
+      });
+      return defer.promise;
+  }).withPaginationType('full_numbers');
+
+  vm.dtColumns = [
+    DTColumnBuilder.newColumn('_id').withTitle('ID').notVisible(),
+    DTColumnBuilder.newColumn('icao').withTitle('ICAO'),
+    DTColumnBuilder.newColumn('name').withTitle('Name'),
+    DTColumnBuilder.newColumn('city').withTitle('City'),
+    DTColumnBuilder.newColumn('country').withTitle('Country'),
+    DTColumnBuilder.newColumn('iata').withTitle('ICAO').notVisible(),
+    DTColumnBuilder.newColumn('lat').withTitle('lat').notVisible(),
+    DTColumnBuilder.newColumn('lon').withTitle('lon').notVisible(),
+    DTColumnBuilder.newColumn('alt').withTitle('alt').notVisible(),
+    DTColumnBuilder.newColumn('utc_offset').withTitle('utc_offset').notVisible(),
+    DTColumnBuilder.newColumn('dst').withTitle('dst').notVisible(),
+    DTColumnBuilder.newColumn('tz').withTitle('tz'),
+    DTColumnBuilder.newColumn('__v').withTitle('__v').notVisible()
+  ];
+};
